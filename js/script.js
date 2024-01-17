51ccc92ce3580f74f0d71203198074cdb9e99ced
BrianBontempi <brian.bont54@gmail.com>
2024-01-17 16:49:38 +0100

    add grid cell and logic for create cell

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// ! recupero documenti dal DOM
+
+const gridElement = document.getElementById("grid")
+const button = document.querySelector("button")
+
+// ! FUNZIONI
+
+const playGame = () => {
+    // svuoto la griglia
+    gridElement.innerText = ' ';
+
+    // Genero le celle e le stampo nella griglia
+    for (let i =1; i <= 100; i++){
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.append(i);
+
+        gridElement.appendChild(cell)
+    }
+}
+
+// ! creo evento del button
+
+button.addEventListener("click", playGame)
